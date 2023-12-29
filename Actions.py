779170439cedfd46945afa8f29376ef15238a33c
PierreRivault <pierre.rivault@protonmail.com>
2023-12-29 23:41:54 +0100

    cleaned up code

--- Actions.py
+++ Actions.py
@@ -1,13 +1,7 @@
 import io
-
-import mutagen
 import glob
-import os
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from PIL import Image
-
-from mutagen.id3 import ID3, APIC
 
 
 def list_mp3_files(folder_var):
@@ -28,7 +22,7 @@ def open_folder(window):
             window.add_file_to_table(file)
 
 
-def image_to_byte_array(image: Image) -> bytes:
+def image_to_byte_array(image):
     # BytesIO is a file-like buffer stored in memory
     imgbytearr = io.BytesIO()
     # image.save expects a file-like as an argument
@@ -36,41 +30,3 @@ def image_to_byte_array(image: Image) -> bytes:
     # Turn the BytesIO object back into a bytes object
     imgbytearr = imgbytearr.getvalue()
     return imgbytearr
-
-
-def save_metadata(window):
-    msg_box = tk.messagebox.askyesno('Sauvegarder les modifications',
-                                     'Êtes-vous sûr de vouloir sauvegarder vos modifications ?', icon='warning')
-    if msg_box:
-        for track_number in range(window.row_count):
-            filename = window.table_values[track_number + 1]['Filename'].get("1.0", "end-1c")
-            try:
-                file = ID3()
-                # metadata saving
-                for head in window.technical_names_table:
-                    file[head] = window.table_values[track_number + 1][head].get(
-                        "1.0", "end-1c").strip()
-                    # Save artist value also in performer to be compatible with more systems
-                    if head == 'artist':
-                        file['performer'] = window.table_values[track_number + 1][head].get("1.0", "end-1c").strip()
-                file.save(window.folder_var + '/' + filename)
-                # Image saving
-                if track_number in window.original_image_table:
-                    audio = ID3(window.folder_var + '/' + filename)
-                    audio.delall('APIC')
-                    audio.add(APIC(encoding=3, mime='image/jpeg', type=3,
-                                   data=image_to_byte_array(window.original_image_table[track_number])))
-                    audio.save(window.folder_var + '/' + filename, v2_version=3)
-                # filename saving
-                title = window.table_values[track_number + 1]['title'].get("1.0", "end-1c")
-                if title:
-                    if window.table_values[track_number + 1]['Filename'] != title.strip() + '.mp3':
-                        os.rename(window.folder_var + '/' + filename, window.folder_var + '/' + title.strip() + '.mp3')
-                        window.table_values[track_number + 1]['Filename'].config(state=tk.NORMAL)
-                        window.table_values[track_number + 1]['Filename'].delete("1.0", tk.END)
-                        window.table_values[track_number + 1]['Filename'].insert(tk.END, title.strip() + '.mp3')
-                        window.table_values[track_number + 1]['Filename'].config(state=tk.DISABLED)
-            except mutagen.MutagenError:
-                tk.messagebox.showerror('File not found', 'File ' + filename + ' not found')
-
-    return
